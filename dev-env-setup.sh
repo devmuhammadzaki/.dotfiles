04c6c06d13389c57392a412841f15c87f55eb5b1
Muhammad Zaki <devmuhammadzaki@gmail.com>
2024-12-14 13:52:14 +0200

    Good message

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -37,6 +37,15 @@ sudo ./aws/install
 echo "Installing Git ..."
 sudo apt install git docker.io -y
 
+# Install MongoDB
+sudo apt-get install gnupg
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
 # Docker Permissions
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
